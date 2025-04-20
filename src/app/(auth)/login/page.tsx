c3e0a51a607558915fcf88d0aa75079df469e1da
roseycomanagement <roseycomanagement@roseyco.co.uk>
2025-04-20 22:41:30 +0000

    update ui login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,12 +7,17 @@ import { supabase } from '@/lib/supabase'
 export default function LoginPage() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        theme="dark"
-        providers={['google']}
-      />
+      <div className="bg-gray-100 rounded-lg p-8 shadow-md w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
+        <div className="mt-10 mb-10">
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            theme="dark"
+            providers={['google']}
+          />
+        </div>
+      </div>
     </div>
   )
 }
